f2605d96ee21f3f94c5ccbc1f9482caff457e2ec
Luchadooor <patgroth@yahoo.com>
2024-11-25 21:05:37 +0100

    Exercise 1 - Defining Structs

--- examples/1-defining-structs/defining/src/main.rs
+++ examples/1-defining-structs/defining/src/main.rs
@@ -1,15 +1,27 @@
-
 #[derive(Debug)]
 struct Person {
     first_name: String,
     last_name: String,
+    email: String,
+    phone: String,
     age: u8,
 }
 
+impl Person {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
 fn main() {
-    println!("{:?}", Person {
-        first_name: "Johny".to_string(),
+    let person = Person {
+        first_name: "John".to_string(),
         last_name: "Doe".to_string(),
+        email: "jD0jw@example.com".to_string(),
+        phone: "123-456-7890".to_string(),
         age: 25,
-    });
+    };
+    println!("The person's full name is: {}", person.full_name());
+    println!("The person's email is: {}", person.email);
+    println!("Again: {:#?}", &person);
 }
