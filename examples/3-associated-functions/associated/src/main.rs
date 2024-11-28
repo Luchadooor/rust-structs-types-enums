862609733ae238988dfd39876edc5f527f6f5e90
Luchadooor <patgroth@yahoo.com>
2024-11-28 11:28:14 +0100

    Example 3 - Associated functions added

--- examples/3-associated-functions/associated/src/main.rs
+++ examples/3-associated-functions/associated/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
@@ -14,8 +15,20 @@ impl User {
             active: true,
         }
     }
+    fn from_email(email: String) -> Self {
+        // Split the email into username and domain
+        // What is the return value of split_once()?
+        // https://doc.rust-lang.org/std/primitive.str.html#method.split_once
+        let (username, domain) = email.split_once('@').expect("Invalid email");
+        // Concatenate the ""https://"" and domain
+        let uri = format!("https://{}", domain);
+        Self::new(String::from(username), email, String::from(uri))
+    }
     fn deactivate(&mut self) {
         self.active = false;
+    }
+    fn update_uri(&mut self, uri: String) {
+        self.uri = uri;
     }
 }
 
@@ -29,4 +42,9 @@ fn main() {
     println!("Account {} status is: {}", new_user.username, new_user.active);
     new_user.deactivate();
     println!("Account {} status is: {}", new_user.username, new_user.active);
+
+    let mut fritz = User::from_email(String::from("fritz@google.com"));
+    println!("Hello, {:#?}!", fritz);
+    fritz.update_uri(String::from("https://fritz.com"));
+    println!("Hello2, {:#?}!", fritz);
 }
