9c24f846e6382039182024df1a3fa9ad37b81080
Luchadooor <patgroth@yahoo.com>
2024-12-01 12:11:27 +0100

    Exercise 9 - Vector Values Solution

--- examples/9-vector-values/values/src/main.rs
+++ examples/9-vector-values/values/src/main.rs
@@ -11,20 +11,33 @@ fn get_item(index: usize) {
 }
 
 fn main() {
-    let vec = vec![1, 2, 3, 4, 5];
-    get_item(3);
+    // let vec = vec![1, 2, 3, 4, 5];
+    // let vec: Vec<i32> = vec![];
+    let vec: Vec<i32> = vec![1,1,1,1,1,1,1,1,1,1,1,1];
+    // get_item(3);
 
     // Retrieve a value at a specific index
-    let third_value = vec[2];
+    // let third_value = vec[2];
     //println!("The third value in the vector is: {}", third_value);
 
     // Retrieve the last value
-    let last_value = vec.last().unwrap();
+    // let last_value = vec.last().unwrap();
     //println!("The last value in the vector is: {}", last_value);
 
-    // Retrieve the first value using pattern matching
-    // match vec.first() {
-    //     Some(first_value) => println!("The first value in the vector is: {}", first_value),
-    //     None => println!("The vector is empty!"),
-    // }
+    //Retrieve the first value using pattern matching
+    match vec.first() {
+        Some(first_value) => println!("The first value in the vector is: {}", first_value),
+        None => println!("The vector is empty!"),
+    }
+
+    let sum = vec_sum(&vec);
+    println!("The sum of the vector is: {}", sum);
+}
+
+fn vec_sum(vec: &Vec<i32>) -> i32 {
+    let mut sum: i32 = 0;
+    for value in vec {
+        sum += value;
+    }
+    sum
 }
