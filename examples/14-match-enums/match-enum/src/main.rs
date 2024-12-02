d6e821b7909daced5cde4c036bd8802822956f64
Luchadooor <patgroth@yahoo.com>
2024-12-02 21:45:11 +0100

    Exercises 14 and 15 Solution

--- examples/14-match-enums/match-enum/src/main.rs
+++ examples/14-match-enums/match-enum/src/main.rs
@@ -3,6 +3,7 @@ enum FileSize {
     Kilobytes(f64),
     Megabytes(f64),
     Gigabytes(f64),
+    Terabytes(f64),
 }
 
 fn format_size(size: u64) -> String {
@@ -10,7 +11,8 @@ fn format_size(size: u64) -> String {
         0..=999 => FileSize::Bytes(size),
         1000..=999_999 => FileSize::Kilobytes(size as f64 / 1000.0),
         1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1_000_000.0),
-        _ => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
+        1_000_000_000..=999_999_999_999 => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
+        _ => FileSize::Terabytes(size as f64 / 1_000_000_000_000.0),
     };
 
     match filesize {
@@ -18,11 +20,47 @@ fn format_size(size: u64) -> String {
         FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
         FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
         FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
+        FileSize::Terabytes(gb) => format!("{:.2} TB", gb),
+    }
+}
+
+fn real_format_size(size: u64) -> String {
+    let filesize = match size {
+        0..=999 => FileSize::Bytes(size),
+        1000..=999_999 => FileSize::Kilobytes(size as f64 / 1024.0),
+        1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1024.0 / 1024.0),
+        1_000_000_000..=999_999_999_999 => FileSize::Gigabytes(size as f64 / 1024.0 / 1024.0 / 1024.0),
+        _ => FileSize::Terabytes(size as f64 / 1024.0 / 1024.0 / 1024.0 / 1024.0),
+    };
+
+    match filesize {
+        FileSize::Bytes(bytes) => format!("{} bytes", bytes),
+        FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
+        FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
+        FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
+        FileSize::Terabytes(gb) => format!("{:.2} TB", gb),
     }
 }
 
 
 fn main() {
-    let result = format_size(6888837399);
-    println!("{}", result)
+    // let result = format_size(6888837399);
+    // println!("{}", result);
+    println!("{}", format_size(12));
+    println!("{}", format_size(12000));
+    println!("{}", format_size(12000000));
+    println!("{}", format_size(12000000000));
+    println!("{}", format_size(12000000000000));
+    println!("{}", format_size(12000000000000000));
+
+    println!("{} -> {}", format_size(12), real_format_size(12));
+    println!("{} -> {}", format_size(12000), real_format_size(12000));
+    println!("{} -> {}", format_size(12000000), real_format_size(12000000));
+    println!("{} -> {}", format_size(12000000000), real_format_size(12000000000));
+    println!("{} -> {}", format_size(12000000000000), real_format_size(12000000000000));
+    println!("{} -> {}", format_size(12000000000000000), real_format_size(12000000000000000));
+    println!("{} -> {}", format_size(2500), real_format_size(2500));
+
+
+
 }
--- examples/15-enums-vectors/enums-vectors/src/main.rs
+++ examples/15-enums-vectors/enums-vectors/src/main.rs
@@ -1,18 +1,69 @@
+#[derive(Debug)]
 enum Shape {
     Circle(f64),
     Square(f64),
+    Triangle(f64, f64, f64),
+}
+
+impl Shape{
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Circle(r) => std::f64::consts::PI * r * r,
+            Shape::Square(s) => s * s,
+            Shape::Triangle(a,b,c) => {let s = (a+b+c)/2f64; return (s*(s-a)*(s-b)*(s-c)).sqrt(); },
+        }
+    }
+}
+
+fn triangle_area(a: f64, b: f64, c: f64) -> f64{
+    let s = (a+b+c)/2f64; 
+    (s*(s-a)*(s-b)*(s-c)).sqrt()
 }
 
 fn main() {
-    let shapes = vec![Shape::Circle(5.0), Shape::Square(3.0)];
-
+    let shapes = vec![Shape::Circle(5.0), Shape::Square(300000.0), Shape::Triangle(100f64,100f64,100f64)];
+    println!("Triangle: {}", shapes[2].area());
     let total_area: f64 = shapes
         .iter()
         .map(|shape| match shape {
             Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
             Shape::Square(length) => length * length,
+            _ => 4.0,
         })
+        .sum();
+    let total_area2: f64 = shapes
+        .iter()
+        .map(|shape| shape.area())
         .sum();
 
     println!("Total area: {} sq. units", total_area);
+    println!("Total area 2: {} sq. units", total_area2);
+    println!("Area: {} sq. units", Shape::Square(5f64).area());
+    println!("Shapes size: {}", shapes.len());
+
+    // shapes.iter().map(|thing| println!("Iter Area: {} sq. units", thing.area()));
+
+    for s in &shapes {
+        println!("For Area:{:?} -> {} sq. units", s, s.area());
+    }
+
+
+    let largest = largest_shape(&shapes);
+    println!("Largest: {:?}", largest);
+
+}
+
+
+fn largest_shape(vec: &Vec<Shape>) -> &Shape{
+    let mut index = 0usize;
+    let mut area = -1f64;
+    for i in 0..vec.len() {
+        if vec[i].area() > area { 
+            area = vec[i].area();
+            index = i;
+         }
+    }
+    let result = &vec[index];
+    return result;
+
 }
