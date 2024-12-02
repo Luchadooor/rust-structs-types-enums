5d6ccf61b2e858a2d3925d43d11e8beeaa05336d
Luchadooor <patgroth@yahoo.com>
2024-12-02 19:15:27 +0100

    Exercise Enum Types Solution

--- examples/10-vector-elements/elements/src/main.rs
+++ examples/10-vector-elements/elements/src/main.rs
@@ -16,4 +16,43 @@ fn main() {
     // insert items at a given index
     v.insert(0, 0);
     println!("{:?}", v); // Output: [0, 1, 2, 3, 4, 5, 6, 7, 8] 
+
+    double_pad(&mut v, 9);
+    println!("{:?}", v);
+
+    let more_numbers = vec![10, 11];
+    combine(&mut v, &more_numbers);
+    println!("{:?}", v);    
+
+    combine_general(&mut v, &more_numbers);
+    println!("{:?}", v);    
+
+    let mut mut_vec = more_numbers.clone();
+    combine_clonefree(&mut v, &mut mut_vec);
+    println!("{:?}", v);    
+
+    let mut vstr = vec!["Hund","Huhn"];
+    let vstr2 = vec!["Ente","Eber","Eidechse"];
+
+    combine_general(&mut vstr, &vstr2);
+    println!("{:?}", vstr);    
+
+
+}
+
+fn double_pad(vec: &mut Vec<i32>, value: i32) {
+    vec.insert(0, value);
+    vec.push(value);    
+}
+
+fn combine(v1: &mut Vec<i32>, v2: &Vec<i32>) {
+    v1.extend(v2);
+}
+
+fn combine_general<T: Clone>(v1: &mut Vec<T>, v2: &Vec<T>) {
+    v1.extend(v2.clone());
+}
+
+fn combine_clonefree<T>(v1: &mut Vec<T>, v2: &mut Vec<T>) {
+    v1.append(v2);
 }
--- examples/12-enum-types/enum-types/src/main.rs
+++ examples/12-enum-types/enum-types/src/main.rs
@@ -6,6 +6,7 @@ enum WineRegions {
     Tuscany,
     Rioja,
     NapaValley,
+    Basel
 }
 
 struct Wine {
@@ -13,7 +14,7 @@ struct Wine {
     region: WineRegions, // wine regions used as a type
 }
 
-fn supported_regions(w: WineRegions) {
+fn supported_regions(w: &WineRegions) {
     match w {
         WineRegions::Rioja => println!("Rioja is supported!"),
         _ => println!("{:?} is not supported!", w),
@@ -31,8 +32,57 @@ fn main() {
         region: WineRegions::Tuscany,
     };
 
-    // println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
-    // println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
-    supported_regions(wine1.region);
-    supported_regions(WineRegions::Rioja);
+    let wine3 = Wine {
+        name: String::from("Heisser Hirsch"),
+        region: WineRegions::Basel,
+    };
+
+    println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
+    println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
+    println!("Wine 3: {} from {:?}", wine3.name, wine3.region);
+    supported_regions(&wine1.region);
+    supported_regions(&WineRegions::Rioja);
+
+    let mut msg = Message::Move { x: 10, y: 20 };
+    process_message(msg);
+    msg = Message::Quit;
+    process_message(msg);
+    msg = Message::Write("Simsalabim!".to_string());
+    process_message(msg);
+    msg = Message::ChangeColor(255,255,255);
+    process_message(msg);
+    msg = Message::AdditionalVariant;
+    process_message(msg);
+
+    println!("Popularity: {}", get_popularity(wine1.region));
+    println!("Popularity: {}", get_popularity(wine2.region));
+    println!("Popularity: {}", get_popularity(wine3.region));
+
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,                       // No associated data
+    Move { x: i32, y: i32 },    // Struct-like data
+    Write(String),              // Tuple-like data
+    ChangeColor(u8, u8, u8),    // Tuple-like data
+    AdditionalVariant,
+}
+
+fn process_message(msg: Message) {
+    match msg {
+        Message::Quit => println!("Quit message received."),
+        Message::Move { x, y } => println!("Move to position: x={}, y={}", x, y),
+        Message::Write(text) => println!("Writing message: {}", text),
+        Message::ChangeColor(r, g, b) => println!("Changing color to RGB({}, {}, {})", r, g, b),
+        _ => println!("Message Variant '{:?}' is not supported.", msg)
+    }
+}
+
+fn get_popularity(wr: WineRegions) -> String {
+    match wr{
+        WineRegions::Basel => return String::from("Most popular wine region on the planet!"),
+        WineRegions::Bordeaux => return String::from("Most popular wine region in France."),
+        _ => return String::from(format!("The wine region of {:?} is nothing compared to the wine region of Basel.", wr)),
+    }
 }
